fd01cc57771bec95b7e9d4fc2b5bf161e0350f57
cya98 <todaudcksgk@naver.com>
2022-12-12 14:46:01 +0900

    last01

--- components/home/hero.js
+++ components/home/hero.js
@@ -14,7 +14,7 @@ export default function hero() {
           안녕하세요! 저는 현재 중부대학교 2학년 정보보호학과에 재학중이며
           next.js, Tailwindcss를 통해 포트폴리오 웹 사이트를 만들어보았습니다.
           하단의 프로젝트 보러가기 버튼을 클릭하시면 웹사이트 프로젝트 팀인
-          '두반장' 팀의 프로젝트인 JBmarket 에대한 주소와 설명을 확인하실 수
+          두반장 팀의 프로젝트인 JBmarket 에대한 주소와 설명을 확인하실 수
           있습니다.
         </p>
         <Link href="/projects">
